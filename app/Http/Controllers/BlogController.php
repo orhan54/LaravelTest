c35d453e15f0cd0b44e16aba3457f837c9e8c07e
CICEK Orhan <orhancicek1985@gmail.com>
2026-03-05 11:56:42 +0100

    Ajout Bootstrap et test Template blade

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -4,23 +4,32 @@ namespace App\Http\Controllers;
 
 use App\Models\Post;
 use Illuminate\Contracts\Pagination\Paginator;
+use Illuminate\View\View;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class BlogController extends Controller
 {
 
-    public function index(): Paginator
+    public function index(): View
     {
-        return Post::paginate(25);
+        return view('blog.index', [
+            'posts' => Post::paginate(1)
+        ]);
     }
 
-    public function show(string $slug, string $id): RedirectResponse | Post
+    public function show(string $slug, string $id): RedirectResponse | View
     {
         $post = Post::findOrFail($id);
-        if ($post->slug === $slug) {
-            return to_route('blog.show', ['slug' => $post->slug, 'id' => $post->id]);
+
+        if ($post->slug !== $slug) {
+            return to_route('blog.show', [
+                'slug' => $post->slug,
+                'id' => $post->id
+            ]);
         }
 
-        return $post;
+        return view('blog.show', [
+            'post' => $post
+        ]);
     }
 }
---
+++ resources/views/base.blade.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous">
+    </script>
+    <style>
+        @layer demo {
+            button {
+                all: unset;
+            }
+        }
+    </style>
+    <title>@yield('title')</title>
+</head>
+
+<body>
+
+    @php
+        $routeName = request()->route()->getName();
+    @endphp
+
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="/">Blog</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a @class(['nav-link', 'active' => str_starts_with($routeName, 'blog.')]) aria-current="page"
+                            href="{{ route('blog.index') }}">Blog</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Link</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+
+    <div class="container">
+
+        @yield('content')
+
+    </div>
+
+
+
+</body>
+
+</html>
